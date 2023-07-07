c00f2db7391028ab2877e521982b687f9dd67657
leguan <leguanYa@gmail.com>
2023-07-07 16:41:13 +0800

    first commit

---
+++ src/main/java/com/leguan/Test.java
@@ -0,0 +1,33 @@
+package com.leguan;
+
+/**
+ * @Description：
+ * @Author：ZhangHui
+ * @Package：com.leguan
+ * @Date: 2023/7/7
+ */
+public class Test {
+
+    private boolean flag = true;
+    private Integer count = 0;
+
+    public void refresh() {
+        flag = false;
+        System.out.println(Thread.currentThread().getName() + "变更flag属性" + flag);
+    }
+
+    public void load() {
+        System.out.println(Thread.currentThread().getName() + "开始执行循环累加");
+        while (flag) {
+            count++;
+        }
+        System.out.println(Thread.currentThread().getName() + "开始执行循环累加结束count=" + count);
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        Test test = new Test();
+        new Thread(test::load, "线程A").start();
+        Thread.sleep(1000);
+        new Thread(test::refresh, "线程B").start();
+    }
+}
