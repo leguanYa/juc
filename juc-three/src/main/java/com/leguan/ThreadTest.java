c8b935744f9d90337a0da203d9ef20e9d2daf106
leguan <leguanYa@gmail.com>
2023-08-17 16:28:55 +0800

    第三天

---
+++ juc-three/src/main/java/com/leguan/ThreadTest.java
@@ -0,0 +1,14 @@
+package com.leguan;
+
+/**
+ * @Description：
+ * @Author：ZhangHui
+ * @Package：com.leguan
+ * @Date: 2023/8/17
+ */
+public class ThreadTest {
+
+    public static void main(String[] args) {
+        System.out.println(1111);
+    }
+}
